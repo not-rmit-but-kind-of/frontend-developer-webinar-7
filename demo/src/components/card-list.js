58e73fe579ba337cfe555c218117166e484bbdad
Ben Howl <benjamin.howl@anz.com>
2019-06-04 08:39:55 +1000

    Convert card-list to use styled-components

--- demo/src/components/card-list.js
+++ demo/src/components/card-list.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
 
-import Card from "./card";
+import Card from './card'
 
-function CardList(props) {
+function CardList (props) {
   return (
-    <div className="destination-card-list">
-      {props.posts.map(function(post, index) {
+    <Wrapper>
+      {props.posts.map(function (post, index) {
         return (
-          <div className="destination-card-list-item" key={index}>
+          <CardListItem key={index}>
             <Card
               title={post.title}
               image={post.image}
               summary={post.summary}
             />
-          </div>
-        );
+          </CardListItem>
+        )
       })}
-    </div>
-  );
+    </Wrapper>
+  )
 }
 
-export default CardList;
+export default CardList
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -0.5rem;
+  margin-left: -0.5rem;
+`
+
+const CardListItem = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 1px;
+  padding-bottom: 0.5rem;
+  padding-top: 0.5rem;
+  padding-right: 0.5rem;
+  padding-left: 0.5rem;
+  flex: 0 0 100%;
+  max-width: 100%;
+
+  @media only screen and (min-width: 480px) {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    flex: 0 0 33.33333%;
+    max-width: 33.33333%;
+  }
+`
