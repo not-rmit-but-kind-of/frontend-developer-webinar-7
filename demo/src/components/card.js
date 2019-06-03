e4c57181fcc005badfbd3b4b7dd81274fdc39fca
Ben Howl <benjamin.howl@anz.com>
2019-06-04 08:34:40 +1000

    Convert card.js to use styled-components

--- demo/src/components/card.js
+++ demo/src/components/card.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
 
-function Card(props) {
+function Card (props) {
   return (
-    <article className="destination-card">
-      <img
-        src={props.image}
-        className="destination-card-image"
-        alt={`Shot of ${props.title}`}
-      />
-      <div className="destination-card-content">
-        <h2 className="destination-card-heading">{props.title}</h2>
-        <p className="destination-card-summary">{props.summary}</p>
-      </div>
-    </article>
-  );
+    <Wrapper>
+      <img src={props.image} alt={`Shot of ${props.title}`} />
+      <Content>
+        <StyledH2>{props.title}</StyledH2>
+        <Summary>{props.summary}</Summary>
+      </Content>
+    </Wrapper>
+  )
 }
 
-export default Card;
+export default Card
+
+const Wrapper = styled.article`
+  border: 1px #ccc solid;
+  border-radius: 5px;
+  background-color: #fff;
+  transition: 0.4s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`
+
+const Content = styled.div`
+  padding: 1.25rem;
+`
+
+const StyledH2 = styled.h2`
+  margin: 0;
+  line-height: 1;
+`
+
+const Summary = styled.p`
+  font-size: 0.9rem;
+`
